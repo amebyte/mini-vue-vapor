393e14a543e53925094b1a8b24f5a0919f6b2b3b
Coboy <416212316@qq.com>
2024-04-26 23:58:41 +0800

    by solid

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
+import { ref, effect } from '@vue/reactivity'
+function template(html) {
+    let node
+    const create = () => {
+        const t = document.createElement("template")
+        t.innerHTML = html
+        return t.content.firstChild
+    }
+    const fn = () => (node || (node = create())).cloneNode(true)
+    return fn 
+}
+
+function render(component, parent) {
+    parent.appendChild(component)
+}
+
+function insert(parent, accessor) {
+    effect(() => {
+        parent.textContent = accessor.value
+    })
+}
+
+// 生成创建 button 标签的函数
+const _tmpl$ = template('<button></button>')
+const App = () => {
+    const count = ref(0)
+    // 真正进行创建模板内容的地方
+    const el = _tmpl$()
+    el.addEventListener('click', () => {
+        count.value++
+    })
+    insert(el, count)
+    return el
+}
 const root = document.getElementById('app')
-root.textContent = '掘金签约作者：Cobyte'
+render(App(), root)
