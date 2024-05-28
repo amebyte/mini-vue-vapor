a28d002f14a72e19f0d261608600b7d1ff3655b6
Coboy <416212316@qq.com>
2024-05-28 23:08:24 +0800

    emit

--- runtime-vapor/src/component.js
+++ runtime-vapor/src/component.js
@@ -37,6 +37,16 @@ export function createSetupContext(instance) {
   return Object.freeze({
     get attrs() {
       return getAttrsProxy(instance)
+    },
+    get emit() {
+      return (event, ...arg) => {
+        // 将事件名转换成 onXXX，on 后面第一个字母需要转换成大写
+        const handleName = `on${event.charAt(0).toUpperCase() + event.slice(1)}`
+        // 在组件实例对象上的 rawProps 判断有没有存在对应的事件，有就执行返回对应的事件函数
+        const handler = instance.rawProps[handleName] && instance.rawProps[handleName]()
+        // 执行在父组件注册的事件函数
+        handler(...arg)
+      }
     }
   })
 }
